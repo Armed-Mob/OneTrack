6e85ede0cbea25910a022e40935de6ff434668d4
Armed Mob <j1034t@gmail.com>
2024-08-13 15:08:01 -0700

    Add Nullables To VehicleDetails Model     Preping For Vehicle Saves To Database

---
+++ OT.Api/Migrations/20240813220635_AddNullablesToVehicleDetails.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OT.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullablesToVehicleDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Turbo",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Trim",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Series",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gvwr",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FuelTypePrimary",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EngineModel",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DriveType",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Doors",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplacementL",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CabType",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Turbo",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Trim",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Series",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gvwr",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FuelTypePrimary",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EngineModel",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DriveType",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Doors",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplacementL",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CabType",
+                table: "VehicleDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- OT.Shared/VehicleDetails.cs
+++ OT.Shared/VehicleDetails.cs
@@ -12,17 +12,17 @@ namespace OT.Shared
         public string Make { get; set; } = string.Empty;
         public string Model { get; set; } = string.Empty;
         public string Year { get; set; } = string.Empty;
-        public string Trim { get; set; } = string.Empty;
-        public string Doors { get; set; } = string.Empty;
-        public string Gvwr { get; set; } = string.Empty;
+        public string? Trim { get; set; } = string.Empty;
+        public string? Doors { get; set; } = string.Empty;
+        public string? Gvwr { get; set; } = string.Empty;
         public int? ColorId { get; set; }
-        public string Series { get; set; } = string.Empty;
-        public string DriveType { get; set; } = string.Empty;
-        public string EngineModel { get; set; } = string.Empty;
-        public string Turbo { get; set; } = string.Empty;
-        public string CabType { get; set; } = string.Empty;
-        public string FuelTypePrimary { get; set; } = string.Empty;
-        public string DisplacementL { get; set; } = string.Empty;
+        public string? Series { get; set; } = string.Empty;
+        public string? DriveType { get; set; } = string.Empty;
+        public string? EngineModel { get; set; } = string.Empty;
+        public string? Turbo { get; set; } = string.Empty;
+        public string? CabType { get; set; } = string.Empty;
+        public string? FuelTypePrimary { get; set; } = string.Empty;
+        public string? DisplacementL { get; set; } = string.Empty;
 
         public int ShopClientId { get; set; }
         public virtual ShopClient ShopClient { get; set; }
